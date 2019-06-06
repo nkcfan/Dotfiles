3f7b2761fcdbd991ef4a67089b1812488a8da3bf
nkcfan <nkcfan@users.noreply.github.com>
2019-06-06 10:51:30 -0700

    Fix pageant command in startup script

--- AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/windows_profile.cmd
+++ AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/windows_profile.cmd
@@ -1,6 +1,6 @@
 SETLOCAL enabledelayedexpansion
 set keylist=
 pushd %USERPROFILE%\.ssh\
-FOR /f "tokens=*" %%f IN ('DIR /B *.ppk') DO SET keylist=%keylist% %%~dpnxf
+FOR /f "tokens=*" %%f IN ('DIR /B *.ppk') DO CALL SET "keylist=%%keylist%% %%~dpnXf"
 popd
 %ChocolateyInstall%\bin\pageant.exe %keylist%
