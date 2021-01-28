e51ddc4dd4ecf3e3d91dda42b6601b7a83cb4dab
nkcfan <nkcfan@users.noreply.github.com>
2021-01-28 07:23:49 +0000

    nvim: move treesitter_config into a separate lua file

---
+++ .config/nvim/lua/treesitter_config.lua
@@ -0,0 +1,65 @@
+require "nvim-treesitter.configs".setup {
+    ensure_installed = {
+        "bash",
+        "cpp",
+        "go",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "yaml"
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = {} -- list of language that will be disabled
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "vii",
+            scope_incremental = "ii",
+            node_incremental = "<CR>",
+            node_decremental = "<S-CR>"
+        }
+    },
+    refactor = {
+        highlight_definitions = {enable = true},
+        highlight_current_scope = {enable = false}
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["ab"] = "@block.outer",
+                ["ib"] = "@block.inner",
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                ["aa"] = "@parameter.outer",
+                ["ia"] = "@parameter.inner",
+                ["cc"] = "@comment.outer",
+                ["ss"] = "@statement.outer",
+                -- Or you can define your own textobjects like this
+                ["iF"] = {
+                    python = "(function_definition) @function",
+                    cpp = "(function_definition) @function",
+                    c = "(function_definition) @function",
+                    java = "(method_declaration) @function"
+                }
+            }
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<LocalLeader><LocalLeader>a"] = "@parameter.inner",
+                ["<LocalLeader><LocalLeader>s"] = "@statement.outer"
+            },
+            swap_previous = {
+                ["<LocalLeader><LocalLeader>A"] = "@parameter.inner",
+                ["<LocalLeader><LocalLeader>S"] = "@statement.outer"
+            }
+        }
+    }
+}
