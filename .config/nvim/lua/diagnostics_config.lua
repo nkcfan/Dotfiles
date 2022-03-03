59290cadafc241b0c85081de2f9e7e9b73a7e541
nkcfan <nkcfan@users.noreply.github.com>
2022-03-03 20:16:24 +0000

    nvim: tune the diagnostic severity for underline, virtual_text and signs

--- .config/nvim/lua/diagnostics_config.lua
+++ .config/nvim/lua/diagnostics_config.lua
@@ -6,3 +6,15 @@ require("diaglist").init({
     -- increase for noisy servers
     debounce_ms = 150,
 })
+
+vim.diagnostic.config({
+    underline = {
+        severity = { min = vim.diagnostic.severity.WARN },
+    },
+    virtual_text = {
+        severity = { min = vim.diagnostic.severity.ERROR },
+    },
+    signs = {
+        severity = { min = vim.diagnostic.severity.HINT },
+    },
+})
