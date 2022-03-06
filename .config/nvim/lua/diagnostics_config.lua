e4d59de7b3e9679191bfa96fbf16e0ca48e50bfa
nkcfan <nkcfan@users.noreply.github.com>
2022-03-06 21:48:09 +0000

    nvim: refine diagnostic toggle: only for current buffer

--- .config/nvim/lua/diagnostics_config.lua
+++ .config/nvim/lua/diagnostics_config.lua
@@ -26,9 +26,9 @@ diagnostic.enabled = true
 diagnostic.toggle = function()
     diagnostic.enabled = not diagnostic.enabled
     if diagnostic.enabled then
-        vim.diagnostic.enable()
+        vim.diagnostic.enable(0, nil)
     else
-        vim.diagnostic.disable()
+        vim.diagnostic.disable(0, nil)
     end
 end
 
