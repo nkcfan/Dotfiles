13fa3df2178bfa8cce0e7f72764fba1d8598a1f4
nkcfan <nkcfan@users.noreply.github.com>
2022-03-30 14:28:32 -0700

    kageant: fix for a hanging window
    Signed-off-by: nkcfan <nkcfan@users.noreply.github.com>

--- AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/windows_profile.cmd
+++ AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/windows_profile.cmd
@@ -1,6 +1,10 @@
 SETLOCAL enabledelayedexpansion
+
+set "agent=kageant.exe"
+tasklist /nh /fi "imagename eq %agent%" | find /i "%agent%" > nul || (start "%agent%" "%agent%" && timeout /t 1 /nobreak)
+
 set keylist=
 pushd %USERPROFILE%\.ssh\
 FOR /f "tokens=*" %%f IN ('DIR /B *.ppk') DO CALL SET "keylist=%%keylist%% %%~dpnXf"
 popd
-%HOMEPATH%\scoop\apps\kitty\current\kageant.exe %keylist%
+"%agent%" %keylist%
