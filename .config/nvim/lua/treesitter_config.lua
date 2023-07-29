f149feef92701ebc28187b4dce074b51dfcc52f0
nkcfan <nkcfan@users.noreply.github.com>
2023-07-29 20:49:12 +0000

    nvim: update goto parameter mappings
    Signed-off-by: nkcfan <nkcfan@users.noreply.github.com>

--- .config/nvim/lua/treesitter_config.lua
+++ .config/nvim/lua/treesitter_config.lua
@@ -108,20 +108,24 @@ require "nvim-treesitter.configs".setup {
         move = {
             enable = true,
             goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
+                ["]f"] = "@function.outer",
+                ["]c"] = "@class.outer",
+                ["]a"] = "@parameter.inner",
             },
             goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
+                ["]F"] = "@function.outer",
+                ["]C"] = "@class.outer",
+                ["]A"] = "@parameter.inner",
             },
             goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
+                ["[f"] = "@function.outer",
+                ["[c"] = "@class.outer",
+                ["[a"] = "@parameter.inner",
             },
             goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
+                ["[F"] = "@function.outer",
+                ["[C"] = "@class.outer",
+                ["[A"] = "@parameter.inner",
             },
         },
     }
