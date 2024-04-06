38fdf094fa3430512a5b2b18b8134e7f4b170c83
nkcfan <nkcfan@users.noreply.github.com>
2024-04-06 08:12:55 -0700

    nvim: add plugin mkdnflow

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'jakewvincent/mkdnflow.nvim',
+        config = function()
+            require('mkdnflow').setup({
+                -- Config goes here; leave blank for defaults
+                mappings = {
+                    MkdnToggleToDo = {{'n', 'v'}, '<localleader>cc'},
+                },
+            })
+        end
+    }
+}
