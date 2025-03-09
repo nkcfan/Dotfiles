b50235052f9faeba296a0ea1c721628e490dfeeb
nkcfan <nkcfan@users.noreply.github.com>
2025-03-08 21:03:31 -0800

    nvim: use render-markdown instead of markview

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,57 +1,12 @@
 return {
     {
-        "OXY2DEV/markview.nvim",
-        lazy = true,     -- false recommended
-        ft = "markdown", -- If you decide to lazy-load anyway
-
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require('markview').setup({
-                checkboxes = {
-                    enable = true,
-
-                    checked = {
-                        --- Text to show
-                        ---@type string
-                        text = "✓",
-
-                        --- Highlight group for "text"
-                        ---@type string?
-                        hl = "MarkviewCheckboxChecked",
-
-                        --- Highlight group to add to the body
-                        --- of the list item.
-                        ---@type string?
-                        scope_hl = nil
-                    },
-
-                    unchecked = {
-                        text = "✘", hl = "MarkviewCheckboxUnchecked",
-                        scope_hl = nil
-                    },
-
-                    custom = {
-                        {
-                            --- Text inside []
-                            ---@type string
-                            match_string = "-",
-
-                            ---@type string
-                            text = "◒",
-
-                            ---@type string?
-                            hl = "MarkviewCheckboxProgress",
-
-                            ---@type string?
-                            scope_hl = nil
-                        }
-                    }
-                }
-            })
-        end
+        'MeanderingProgrammer/render-markdown.nvim',
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
     },
     {
         'jakewvincent/mkdnflow.nvim',
