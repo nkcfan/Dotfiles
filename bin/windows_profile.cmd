a4a09354dcf0efcddb7a9e10855394d2bc201cc8
nkcfan <nkcfan@users.noreply.github.com>
2026-01-08 18:18:04 -0800

    win: set XDG_CONFIG_HOME, PATH, and nvim wrapper

--- bin/windows_profile.cmd
+++ bin/windows_profile.cmd
@@ -1,9 +1,15 @@
 SETLOCAL enabledelayedexpansion
 
+REM Start key agent and load all the keys
 set "agent=kageant.exe"
-
 set keylist=
 pushd %USERPROFILE%\.ssh\
 FOR /f "tokens=*" %%f IN ('DIR /B *.ppk') DO CALL SET "keylist=%%keylist%% %%~dpnXf"
 popd
 "%agent%" %keylist%
+
+REM Set env var
+setx XDG_CONFIG_HOME "%%USERPROFILE%%\.config"
+
+REM Set path
+setx PATH "%PATH%;%%USERPROFILE%%\bin"
